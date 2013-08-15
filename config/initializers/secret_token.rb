b8cc3808e95216b661c8f2e711c3973b60d69509
Luis Luna Aguilar <luis@Luiss-MacBook-Air.local>
2013-08-15 17:27:28 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ASimpleCorner::Application.config.secret_key_base = '7260de6cd817d959d8e12b6b3d6f7a23833ae9ae27542d1abf44cc89269cf32f4cc1cedbb31aabf5f3d16f76d111570e534bf2d56da2cfaf3e8dc3657141320d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ASimpleCorner::Application.config.session_store :cookie_store, key: '_a_simple_corner_session'
