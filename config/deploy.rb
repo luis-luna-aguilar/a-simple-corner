e315eba520a528d616d0a18e785db259485be170
Luis Luna <luislunaaguilar@gmail.com>
2013-09-19 17:18:19 -0600

    Deployment additions

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,15 +8,46 @@ set :scm_passphrase, ""
 
 set :user, "root"
 set :use_sudo, false
+set :deploy_to, "/var/www/a_simple_corner"
 
 set :stages, ["production"]
 set :default_stage, "production"
 
 set :normalize_asset_timestamps, false
 
-after "deploy", "deploy:extra_symlinking"
+set :current_path, "#{deploy_to}/current"
+set :shared_path, "#{deploy_to}/shared"
+
+set :unicorn_binary, "bundle exec unicorn_rails"
+set :unicorn_config, "#{current_path}/config/unicorn.rb"
+set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
+
+set :rvm_gems_path, "/usr/local/rvm/"
+
+set :default_environment, {
+  'PATH' => "#{rvm_gems_path}gems/ruby-2.0.0-p195@a_simple_corner/bin:#{rvm_gems_path}gems/ruby-2.0.0-p195@global/bin:#{rvm_gems_path}gems/ruby-2.0.0-p195/bin:#{rvm_gems_path}bin:$PATH",
+  'RUBY_VERSION' => 'ruby 2.0.0',
+  'GEM_HOME'     => '/usr/local/rvm/gems/ruby-2.0.0-p195@a_simple_corner',
+  'GEM_PATH'     => '/usr/local/rvm/gems/ruby-2.0.0-p195@a_simple_corner',
+  'BUNDLE_PATH'  => '/usr/local/rvm/gems/ruby-2.0.0-p195@a_simple_corner'
+}
+
+after "deploy:symlink", "deploy:extra_symlinking"
 
 namespace :deploy do
+
+  task :start do 
+    run "cd #{current_path} && #{unicorn_binary} -E #{rails_env} -c #{unicorn_config} -D"
+  end
+
+  task :stop do
+    (run "kill `cat #{unicorn_pid}`") rescue true
+  end
+
+  task :restart, :roles => :app do
+    stop
+    start
+  end
 
   task :extra_symlinking, :roles => :app do
     run "ln -s #{shared_path}/database.yml #{current_path}/config/database.yml"
